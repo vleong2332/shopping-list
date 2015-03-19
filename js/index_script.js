1cdbb594ed454fe20dbf116159c4280c962ca0f1
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-03-18 23:14:31 -0500

    A> Add new item functionality

--- js/index_script.js
+++ js/index_script.js
@@ -1,6 +1,12 @@
 $(document).ready( function() {
 	console.log("Doc is ready");
 
+	// RESET ADD_ITEM INPUT
+	$('.left_icon .flaticon-delete101').click( function() {
+		$('#add_item').val('');
+		$('#add_item').focus();
+	});
+
 	// CHECKING-UNCHECKING ITEM
 	$('#item_list').on('click', '.left_icon > i', function() {
 		$(this).toggleClass("flaticon-pencil122");
@@ -8,3 +14,21 @@ $(document).ready( function() {
 	 	$(this).parent().parent().toggleClass("checked");
 	});
 });
+
+// ADDING TO THE LIST
+$.fn.addNewItem = function() {
+	var input = '#add_item';
+	if (($(input).val() !== "") && ($(input).val() !== "Add item . . ."))
+	{
+		var new_item = "<li>" +
+						"<div class=\"left_icon\"><i class=\"flaticon-pencil122\"></i></div>" +
+						"<p>" +	$(input).val() + "</p>" +
+						"<div class=\"right_icon\"><i class=\"flaticon-recycling10\"></i></div>" +
+						"<div style=\"clear: both\"></div>" +
+						"</li>";
+		$('#item_list').prepend(new_item);
+		$(input).val("").focus();
+	}
+	console.log("addNewItem triggered, but missed if conditions");
+	return;
+};
